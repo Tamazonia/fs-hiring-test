e40ee833b296dd915506d597b7ddb60bbec0cced
Tamara Jost <facebookoftjo@gmail.com>
2018-01-27 19:48:07 +0000

    pundit implemented for show and index of motels

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.3.3'
 
 gem 'devise'
+gem "pundit"
 gem 'figaro'
 gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
--- app/controllers/motels_controller.rb
+++ app/controllers/motels_controller.rb
@@ -2,6 +2,7 @@ class MotelsController < ApplicationController
 
   def show
     @motel = Motel.find(params[:id])
+    authorize @motel
     @rooms = @motel.rooms
     @earnings = @rooms.joins(:bookings).sum(:amount_centavos_earned)
   end
@@ -9,6 +10,7 @@ class MotelsController < ApplicationController
   def index
     @user = current_user
     @motels = @user.motels
+    @motels = policy_scope(Motel)
     @earnings = @motels.joins(:bookings).sum(:amount_centavos_earned)
   end
 end
