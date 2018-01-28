007f039233528ab515aec4beae072bfdeb3d3c15
Tamara Jost <facebookoftjo@gmail.com>
2018-01-28 20:31:34 +0000

    first test set up

---
+++ test/system/pages_test.rb
@@ -0,0 +1,8 @@
+require "application_system_test_case"
+
+class PagesTest < ApplicationSystemTestCase
+  # test "visiting the pages" do
+  #   visit "/"
+  #   assert_selector "h1", text: "O FUTURO FINALMENTE CHEGOU AOS MOTÉIS"
+  # end
+end
