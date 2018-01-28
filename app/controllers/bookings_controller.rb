ae4372e5ca9e8fe41759b640c06b2e5ae6414fc2
Tamara Jost <facebookoftjo@gmail.com>
2018-01-28 19:28:22 +0000

    no motel sites for bookings view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,9 @@
 class BookingsController < ApplicationController
 
   def index
-    # @motel = Motel.find(params[:motel_id])
-
     @user = current_user
     @bookings = @user.bookings
     @bookings = policy_scope(Booking)
-
-    # @user.bookings =
-    # @bookings = @motel.bookings
-    # @bookings = policy_scope(Booking)
-
   end
 
 end
